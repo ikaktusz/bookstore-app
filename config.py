78f1b3a44e8d0d610587ff25cb3e05219a9e3ef3
Konstantin <bamberger.konstantin@gmail.com>
2021-03-03 01:30:56 +0300

    change config file

--- config.py
+++ config.py
@@ -8,5 +8,9 @@ load_dotenv(dotenv_path)
 
 class Config:
     '''Set flask configuration from .env file.'''
-
+    #Flask config
     SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    #Sqlalchemy config
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
